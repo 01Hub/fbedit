b5e86eb3cf90a8b74cc9ce864e5e5f83cee11d39
ketilo <ketilo@2a7fbd6b-f62f-0410-8f40-cc077eb123af>
2007-06-19 21:18:01 +0000

    Added batch file to copy custom controls to fbedit root
    git-svn-id: https://svn.code.sf.net/p/fbedit/code@160 2a7fbd6b-f62f-0410-8f40-cc077eb123af

---
+++ GetCustomControls.bat
@@ -0,0 +1,7 @@
+Copy CodeComplete\RACodeComplete.dll \FbEdit
+Copy FileBrowser\RAFile.dll \FbEdit
+Copy Grid\RAGrid.dll \FbEdit
+Copy Property\RAProperty.dll \FbEdit
+Copy ResEd\Dll\RAResEd.dll \FbEdit
+Copy SimEd\RAEdit.dll \FbEdit
+pause
