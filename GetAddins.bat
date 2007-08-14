dc49380f20c4849387de30fb7ada855168d70de1
KetilO <KetilO@2a7fbd6b-f62f-0410-8f40-cc077eb123af>
2007-08-14 09:56:17 +0000

    Added CustomFontAddin
    git-svn-id: https://svn.code.sf.net/p/fbedit/code@226 2a7fbd6b-f62f-0410-8f40-cc077eb123af

--- GetAddins.bat
+++ GetAddins.bat
@@ -1,3 +1,10 @@
 Copy Addins\AdvEdit\AdvEdit.dll \FbEdit\Addins
+Copy Addins\AdvEdit\AdvEdit.txt \FbEdit\Addins\Help
+
 Copy Addins\SnipletAddin\SnipletAddin.dll \FbEdit\Addins
+Copy Addins\SnipletAddin\SnipletAddin.txt \FbEdit\Addins\Help
+
+Copy Addins\CustomFontAddin\CustomFontAddin.dll \FbEdit\Addins
+Copy Addins\CustomFontAddin\CustomFontAddin.txt \FbEdit\Addins\Help
+
 Pause
