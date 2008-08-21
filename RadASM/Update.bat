ebfb5038737bd834aa82a899f470cdceabf156ad
ketilo <ketilo@2a7fbd6b-f62f-0410-8f40-cc077eb123af>
2008-08-21 22:00:21 +0000

    Updated RadASM
    git-svn-id: https://svn.code.sf.net/p/fbedit/code@654 2a7fbd6b-f62f-0410-8f40-cc077eb123af

---
+++ RadASM/Update.bat
@@ -0,0 +1,15 @@
+dir Addins\PthExpl++\*.*
+copy Addins\DlgToWin\DlgToWin.dll C:\RadASM\Addins
+copy Addins\Preview\Preview.dll C:\RadASM\Addins
+copy Addins\PthExpl++\PthExpl++.dll C:\RadASM\Addins
+copy Addins\ResourceID\ResourceID.dll C:\RadASM\Addins
+
+copy Addins\DlgToWin\DlgToWin.txt C:\RadASM\Addins\Help
+copy Addins\Preview\Preview.txt C:\RadASM\Addins\Help
+copy Addins\PthExpl++\PthExpl++.txt C:\RadASM\Addins\Help
+copy Addins\ResourceID\ResourceID.txt C:\RadASM\Addins\Help
+
+copy RadASM.exe C:\RadASM
+copy RadLNG.exe C:\RadASM\Language
+
+pause
