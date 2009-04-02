89937824fe23deb2d9504f4721964947bd1acede
ketilo <ketilo@2a7fbd6b-f62f-0410-8f40-cc077eb123af>
2009-04-02 00:23:55 +0000

    Updated MasmEd
    git-svn-id: https://svn.code.sf.net/p/fbedit/code@969 2a7fbd6b-f62f-0410-8f40-cc077eb123af

--- MasmEd/MasmEd/Opt/BuildOption.asm
+++ MasmEd/MasmEd/Opt/BuildOption.asm
@@ -1,17 +1,91 @@
 
-IDD_PATHOPTION					equ 3500
-IDC_EDTBIN						equ 1001
-IDC_EDTINC						equ 1002
-IDC_EDTLIB						equ 1003
-IDC_BTNPATHRESTORE				equ 1004
+IDD_PATHOPTION		equ 3500
+IDC_EDTBIN			equ 1001
+IDC_EDTINC			equ 1002
+IDC_EDTLIB			equ 1003
+IDC_BTNPATHRESTORE	equ 1004
 
-IDD_BUILDOPTION					equ 3400
-IDC_EDTRES						equ 1001
-IDC_EDTASM						equ 1002
-IDC_EDTLNK						equ 1003
-IDC_BTNRESTORE					equ 1004
+IDD_BUILDOPTION		equ 3400
+IDC_EDTRES			equ 1001
+IDC_EDTASM			equ 1002
+IDC_EDTLNK			equ 1003
+IDC_BTNRESTORE		equ 1004
+
+.const
+
+szPath				db 'path',0
+szInclude			db 'include',0
+szIncludelib		db 'lib',0
+
+.data?
+
+hEnv				dd ?
+hEnvMem				dd ?
+pNextVal			dd ?
 
 .code
+
+ResetEnvironment proc uses esi edi
+
+	mov		edi,hEnv
+	.if	edi
+		.while byte	ptr	[edi]
+			mov		esi,edi
+			invoke lstrlen,esi
+			lea		esi,[esi+eax+1]
+			invoke SetEnvironmentVariable,edi,esi
+			invoke lstrlen,esi
+			lea		esi,[esi+eax+1]
+			mov		edi,esi
+		.endw
+		invoke GlobalFree,hEnv
+		xor		eax,eax
+		mov		hEnv,eax
+	.endif
+	ret
+
+ResetEnvironment endp
+
+SetVar proc uses edi,lpSave:DWORD,lpName:DWORD,lpValue:DWORD
+
+	mov		edi,lpSave
+	mov		byte ptr tmpbuff[4096],0
+	invoke GetEnvironmentVariable,lpName,addr tmpbuff[4096],1024
+	invoke lstrcpy,edi,lpName
+	invoke lstrlen,edi
+	lea		edi,[edi+eax+1]
+	invoke lstrcpy,edi,addr tmpbuff[4096]
+	invoke lstrlen,edi
+	lea		edi,[edi+eax+1]
+	invoke lstrcpy,addr tmpbuff,lpValue
+	.if byte ptr tmpbuff[4096]
+		invoke lstrcat,addr tmpbuff,addr szSemi
+		invoke lstrcat,addr tmpbuff,addr tmpbuff[4096]
+	.endif
+	invoke SetEnvironmentVariable,lpName,addr tmpbuff
+	ret
+
+SetVar endp
+
+SetEnvironment proc uses edi
+	LOCAL	lpEnv:DWORD
+
+	;Environment
+	invoke ResetEnvironment
+	invoke GlobalAlloc,GMEM_FIXED or GMEM_ZEROINIT,16384
+	mov		hEnv,eax
+	mov		edi,eax
+	invoke GetEnvironmentStrings
+	mov		lpEnv,eax
+	invoke SetVar,edi,addr szPath,addr PathBin
+	mov		edi,eax
+	invoke SetVar,edi,addr szInclude,addr PathInc
+	mov		edi,eax
+	invoke SetVar,edi,addr szIncludelib,addr PathLib
+	mov		edi,eax
+	ret
+
+SetEnvironment endp
 
 PathOptionDialogProc proc hWin:HWND,uMsg:UINT,wParam:WPARAM,lParam:LPARAM
 
@@ -41,6 +115,7 @@ PathOptionDialogProc proc hWin:HWND,uMsg:UINT,wParam:WPARAM,lParam:LPARAM
 				invoke lstrlen,addr PathLib
 				inc		eax
 				invoke RegSetValueEx,hReg,addr szPathLib,0,REG_SZ,addr PathLib,eax
+				invoke SetEnvironment
 				invoke SendMessage,hWin,WM_CLOSE,NULL,NULL
 			.elseif eax==IDCANCEL
 				invoke SendMessage,hWin,WM_CLOSE,NULL,NULL
