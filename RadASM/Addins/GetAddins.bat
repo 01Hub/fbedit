fed49c9f32f80420a7ebbc78da8073e1bbd5939b
ketilo <ketilo@2a7fbd6b-f62f-0410-8f40-cc077eb123af>
2009-09-23 09:08:05 +0000

    Updated RadASM 2218
    git-svn-id: https://svn.code.sf.net/p/fbedit/code@1310 2a7fbd6b-f62f-0410-8f40-cc077eb123af

--- RadASM/Addins/GetAddins.bat
+++ RadASM/Addins/GetAddins.bat
@@ -33,4 +33,35 @@ copy StyleMana\StyleMana.dll \RadASM\Addins
 copy TbrCreate\TbrCreate.exe \RadASM\Addins
 rem copy \.dll \RadASM\Addins
 
+rem -------------------------------------------------------
+
+copy AddinMana\AddinMana.txt \RadASM\Addins\Help
+copy AdvEdit\AdvEdit.txt \RadASM\Addins\Help
+copy AlexMenu\AlexMenu.txt \RadASM\Addins\Help
+copy Collapse\Collapse.txt \RadASM\Addins\Help
+
+copy CompileTime\CompileTime.txt \RadASM\Addins\Help
+copy CreateProtoEx\CreateProtoEx.txt \RadASM\Addins\Help
+copy CtrlNames\CtrlNames.txt \RadASM\Addins\Help
+
+copy DlgToWin\DlgToWin.txt \RadASM\Addins\Help
+copy FlipCase\FlipCase.txt \RadASM\Addins\Help
+
+copy Preview\Preview.txt \RadASM\Addins\Help
+copy ProjectTimer\ProjectTimer.txt \RadASM\Addins\Help
+copy ProjectZip\ProjectZip.txt \RadASM\Addins\Help
+
+copy PthExpl\PthExpl*.txt \RadASM\Addins\Help
+copy RADbg\RADbg.txt \RadASM\Addins\Help
+copy RADebug\RADebug.txt \RadASM\Addins\Help
+copy RadFavs\RadFavs.txt \RadASM\Addins\Help
+
+copy RadHelp\RadHelp.txt \RadASM\Addins\Help
+copy ReallyRad\ReallyRad.txt \RadASM\Addins\Help
+copy ResourceID\ResourceID.txt \RadASM\Addins\Help
+
+copy SourceSafe\SourceSafe.txt \RadASM\Addins\Help
+copy StyleMana\StyleMana.txt \RadASM\Addins\Help
+rem copy \.txt \RadASM\Addins\Help
+
 pause
