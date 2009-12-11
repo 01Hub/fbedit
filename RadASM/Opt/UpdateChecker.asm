0e0736f19f685143a14e5347fd7e4e6ccea3e48d
ketilo <ketilo@2a7fbd6b-f62f-0410-8f40-cc077eb123af>
2009-12-11 08:07:35 +0000

    Updated RadASM 2218g
    git-svn-id: https://svn.code.sf.net/p/fbedit/code@1462 2a7fbd6b-f62f-0410-8f40-cc077eb123af

--- RadASM/Opt/UpdateChecker.asm
+++ RadASM/Opt/UpdateChecker.asm
@@ -5,9 +5,13 @@ IDC_CHKGETIDE					equ 1002
 IDC_CHKGETASM					equ 1003
 IDC_CHKGETHLL					equ 1004
 IDC_CHKGETLNG					equ 1005
+IDC_BTNDOWNLOAD					equ 1008
+IDC_EDTDLPATH					equ 1006
+IDC_BTNDLPATH					equ 1007
 
 IDD_DLGDOWNLOAD					equ 8100
 IDC_STCDOWNLOADING				equ 1001
+IDC_STCFILESIZE					equ 1003
 IDC_PGB1						equ 1002
 
 .const
@@ -17,8 +21,10 @@ szINetErr4						db 'Could not find:',13,10
 szUrlVersion					db 'https://fbedit.svn.sourceforge.net/svnroot/fbedit/RadASM/ReleaseVersion.txt',0
 szUrlFile						db 'https://fbedit.svn.sourceforge.net/svnroot/fbedit/RadASM/ReleaseMake/',0
 
-szIDEFile						db 'Release.zip',0
-;szIDEFile						db 'RadASMIDE.zip',0
+szDownloading					db 'Downloading: ',0
+szFmtFilesize					db 'Filesize: %d bytes',0
+;szIDEFile						db 'Release.zip',0
+szIDEFile						db 'RadASMIDE.zip',0
 szASMFile						db 'Assembly.zip',0
 szHLLFile						db 'HighLevel.zip',0
 szLNGFile						db 'Language.zip',0
@@ -26,6 +32,11 @@ szLNGFile						db 'Language.zip',0
 szINetErr1						db 'InternetOpen failed.',0
 szINetErr2						db 'InternetOpenUrl failed.',0
 szINetErr3						db 'InternetReadFile failed',0
+
+.data?
+
+szDLFileName					db MAX_PATH dup(?)
+szDLPath						db MAX_PATH dup(?)
 
 .code
 
@@ -39,9 +50,8 @@ InternetDownloadFile proc uses ebx,hWin:HWND
 	LOCAL	dwread:DWORD
 	LOCAL	dwindex:DWORD
 
-	invoke GetDlgItemText,hWin,IDC_STCDOWNLOADING,addr buffer1,sizeof buffer1
 	invoke strcpy,addr buffer,addr szUrlFile
-	invoke strcat,addr buffer,addr buffer1
+	invoke strcat,addr buffer,addr szDLFileName
 
 	invoke InternetOpen,addr AppName,INTERNET_OPEN_TYPE_DIRECT,0,0,0
 	.if eax
@@ -49,9 +59,11 @@ InternetDownloadFile proc uses ebx,hWin:HWND
 		invoke InternetOpenUrl,hInternet,addr buffer,0,0,INTERNET_FLAG_RELOAD or INTERNET_FLAG_NO_CACHE_WRITE,addr contextid
 		.if eax
 			mov		hUrl,eax
-			mov		dwread,256
+			mov		dwread,4
 			invoke HttpQueryInfo,hUrl,HTTP_QUERY_CONTENT_LENGTH or HTTP_QUERY_FLAG_NUMBER,addr dwsize,addr dwread,addr dwindex
 			mov		ebx,dwsize
+			invoke wsprintf,addr buffer1,addr szFmtFilesize,ebx
+			invoke SetDlgItemText,hWin,IDC_STCFILESIZE,addr buffer1
 			shr		ebx,8
 			shl		ebx,16
 			invoke SendDlgItemMessage,hWin,IDC_PGB1,PBM_SETRANGE,0,ebx
@@ -89,11 +101,14 @@ InternetDownloadFile proc uses ebx,hWin:HWND
 InternetDownloadFile endp
 
 DownloadProc proc hWin:HWND,uMsg:UINT,wParam:WPARAM,lParam:LPARAM
+	LOCAL	buffer[MAX_PATH]:BYTE
 	LOCAL	tid:DWORD
 
 	mov		eax,uMsg
 	.if eax==WM_INITDIALOG
-		invoke SetDlgItemText,hWin,IDC_STCDOWNLOADING,lParam
+		invoke strcpy,addr buffer,addr szDownloading
+		invoke strcat,addr buffer,addr szDLFileName
+		invoke SetDlgItemText,hWin,IDC_STCDOWNLOADING,addr buffer
 		invoke CreateThread,NULL,NULL,addr InternetDownloadFile,hWin,NORMAL_PRIORITY_CLASS,addr tid
 	.elseif eax==WM_COMMAND
 		mov		edx,wParam
@@ -117,7 +132,8 @@ DownloadProc endp
 
 InternetDownload proc hWin:HWND,lpFileName:DWORD
 
-	invoke ModalDialog,hInstance,IDD_DLGDOWNLOAD,hWin,addr DownloadProc,lpFileName
+	invoke strcpy,addr szDLFileName,lpFileName
+	invoke ModalDialog,hInstance,IDD_DLGDOWNLOAD,hWin,addr DownloadProc,0
 	ret
 
 InternetDownload endp
@@ -159,42 +175,54 @@ InternetGetVersion proc lpUrl:DWORD,lpBuff:DWORD,nBytes:DWORD
 InternetGetVersion endp
 
 UpdateCheckerProc proc hWin:HWND,uMsg:UINT,wParam:WPARAM,lParam:LPARAM
+	LOCAL	buffer[MAX_PATH]:BYTE
 
 	mov		eax,uMsg
 	.if eax==WM_INITDIALOG
-		invoke RtlZeroMemory,addr tempbuff,sizeof tempbuff
-		invoke InternetGetVersion,addr szUrlVersion,addr tempbuff,1023
-		.if eax==-1
-			mov		eax,offset szINetErr1
-		.elseif eax==-2
-			mov		eax,offset szINetErr2
-		.elseif eax==-3
-			mov		eax,offset szINetErr3
-		.else
-			.if word ptr tempbuff=='!<'
-				mov		eax,offset szINetErr4
-			.else
-				mov		eax,IDC_CHKGETIDE
-				call	Enable
-				mov		eax,IDC_CHKGETASM
-				call	Enable
-				mov		eax,IDC_CHKGETHLL
-				call	Enable
-				mov		eax,IDC_CHKGETLNG
-				call	Enable
-				mov		eax,IDOK
-				call	Enable
-				invoke wsprintf,addr tempbuff[1024],addr szFmtVersion,addr AppName,addr tempbuff
-				lea		eax,tempbuff[1024]
-			.endif
-		.endif
-		invoke SetDlgItemText,hWin,IDC_STCVERSION,eax
+		invoke iniGetAppPath,addr buffer
+		invoke SetDlgItemText,hWin,IDC_EDTDLPATH,addr buffer
 	.elseif eax==WM_COMMAND
 		mov		edx,wParam
 		movzx	eax,dx
 		shr		edx,16
 		.if edx==BN_CLICKED
 			.if eax==IDOK
+				invoke RtlZeroMemory,addr tempbuff,sizeof tempbuff
+				invoke InternetGetVersion,addr szUrlVersion,addr tempbuff,1023
+				.if eax==-1
+					mov		eax,offset szINetErr1
+				.elseif eax==-2
+					mov		eax,offset szINetErr2
+				.elseif eax==-3
+					mov		eax,offset szINetErr3
+				.else
+					.if word ptr tempbuff=='!<'
+						mov		eax,offset szINetErr4
+					.else
+						mov		eax,IDC_CHKGETIDE
+						call	Enable
+						mov		eax,IDC_CHKGETASM
+						call	Enable
+						mov		eax,IDC_CHKGETHLL
+						call	Enable
+						mov		eax,IDC_CHKGETLNG
+						call	Enable
+						mov		eax,IDC_EDTDLPATH
+						call	Enable
+						mov		eax,IDC_BTNDLPATH
+						call	Enable
+						invoke GetDlgItem,hWin,IDOK
+						invoke ShowWindow,eax,SW_HIDE
+						invoke GetDlgItem,hWin,IDC_BTNDOWNLOAD
+						invoke ShowWindow,eax,SW_SHOW
+						invoke wsprintf,addr tempbuff[1024],addr szFmtVersion,addr AppName,addr tempbuff
+						lea		eax,tempbuff[1024]
+					.endif
+				.endif
+				invoke SetDlgItemText,hWin,IDC_STCVERSION,eax
+			.elseif eax==IDCANCEL
+				invoke SendMessage,hWin,WM_CLOSE,NULL,NULL
+			.elseif eax==IDC_BTNDOWNLOAD
 				invoke IsDlgButtonChecked,hWin,IDC_CHKGETIDE
 				.if eax
 					invoke InternetDownload,hWin,addr szIDEFile
@@ -208,8 +236,6 @@ UpdateCheckerProc proc hWin:HWND,uMsg:UINT,wParam:WPARAM,lParam:LPARAM
 				invoke IsDlgButtonChecked,hWin,IDC_CHKGETLNG
 				.if eax
 				.endif
-			.elseif eax==IDCANCEL
-				invoke SendMessage,hWin,WM_CLOSE,NULL,NULL
 			.endif
 		.endif
 	.elseif eax==WM_CLOSE
