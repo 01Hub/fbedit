e2e049be357521d395e1df7f7ea758ffa52b7ef1
ketilo <ketilo@2a7fbd6b-f62f-0410-8f40-cc077eb123af>
2011-11-30 00:09:11 +0000

    git-svn-id: https://svn.code.sf.net/p/fbedit/code@2275 2a7fbd6b-f62f-0410-8f40-cc077eb123af

---
+++ Sim52/Sim52.asm
@@ -0,0 +1,116 @@
+.386
+.model flat,stdcall
+option casemap:none
+
+include Sim52.inc
+include Sim52Parse.asm
+include Sim52Core.asm
+
+.code
+
+WndProc proc hWin:HWND,uMsg:UINT,wParam:WPARAM,lParam:LPARAM
+	LOCAL	ofn:OPENFILENAME
+	LOCAL	buffer[MAX_PATH]:BYTE
+
+	mov		eax,uMsg
+	.if eax==WM_INITDIALOG
+		mov		eax,hWin
+		mov		hWnd,eax
+		; Create font and set it to list box
+		invoke CreateFontIndirect,addr Courier_New_9
+		mov		hLstFont,eax
+		invoke SendDlgItemMessage,hWin,IDC_LSTCODE,WM_SETFONT,hLstFont,FALSE
+	.elseif eax==WM_COMMAND
+		mov		eax,wParam
+		and		eax,0FFFFh
+		.if eax==IDM_FILE_EXIT
+			invoke SendMessage,hWin,WM_CLOSE,0,0
+		.elseif eax==IDM_FILE_OPEN
+			;Zero out the ofn struct
+			invoke RtlZeroMemory,addr ofn,sizeof ofn
+			;Setup the ofn struct
+			mov		ofn.lStructSize,sizeof ofn
+			push	hWin
+			pop		ofn.hwndOwner
+			push	hInstance
+			pop		ofn.hInstance
+			mov		ofn.lpstrFilter,offset szLSTFilterString
+			mov		buffer[0],0
+			lea		eax,buffer
+			mov		ofn.lpstrFile,eax
+			mov		ofn.nMaxFile,sizeof buffer
+			mov		ofn.lpstrDefExt,NULL
+			mov		ofn.Flags,OFN_FILEMUSTEXIST or OFN_HIDEREADONLY or OFN_PATHMUSTEXIST
+			;Show the Open dialog
+			invoke GetOpenFileName,addr ofn
+			.if eax
+				invoke ParseList,addr buffer
+			.endif
+		.elseif eax==IDM_HELP_ABOUT
+			invoke ShellAbout,hWin,addr AppName,addr AboutMsg,NULL
+		.endif
+	.elseif eax==WM_CLOSE
+		.if hMemFile
+			invoke GlobalFree,hMemFile
+		.endif
+		.if hMemCode
+			invoke GlobalFree,hMemCode
+		.endif
+		invoke DeleteObject,hLstFont
+		invoke DestroyWindow,hWin
+	.elseif uMsg==WM_DESTROY
+		invoke PostQuitMessage,NULL
+	.else
+		invoke DefWindowProc,hWin,uMsg,wParam,lParam
+		ret
+	.endif
+	xor    eax,eax
+	ret
+
+WndProc endp
+
+WinMain proc hInst:HINSTANCE,hPrevInst:HINSTANCE,CmdLine:LPSTR,CmdShow:DWORD
+	LOCAL	wc:WNDCLASSEX
+	LOCAL	msg:MSG
+
+	mov		wc.cbSize,sizeof WNDCLASSEX
+	mov		wc.style,CS_HREDRAW or CS_VREDRAW
+	mov		wc.lpfnWndProc,offset WndProc
+	mov		wc.cbClsExtra,NULL
+	mov		wc.cbWndExtra,DLGWINDOWEXTRA
+	push	hInst
+	pop		wc.hInstance
+	mov		wc.hbrBackground,COLOR_BTNFACE+1
+	mov		wc.lpszMenuName,IDM_MENU
+	mov		wc.lpszClassName,offset ClassName
+	invoke LoadIcon,NULL,IDI_APPLICATION
+	mov		wc.hIcon,eax
+	mov		wc.hIconSm,eax
+	invoke LoadCursor,NULL,IDC_ARROW
+	mov		wc.hCursor,eax
+	invoke RegisterClassEx,addr wc
+	invoke CreateDialogParam,hInstance,IDD_SIM52,NULL,addr WndProc,NULL
+	invoke ShowWindow,hWnd,SW_SHOWNORMAL
+	invoke UpdateWindow,hWnd
+	.while TRUE
+		invoke GetMessage,addr msg,NULL,0,0
+	  .BREAK .if !eax
+		invoke TranslateMessage,addr msg
+		invoke DispatchMessage,addr msg
+	.endw
+	mov		eax,msg.wParam
+	ret
+
+WinMain endp
+
+start:
+
+	invoke GetModuleHandle,NULL
+	mov    hInstance,eax
+	invoke GetCommandLine
+	invoke InitCommonControls
+	mov		CommandLine,eax
+	invoke WinMain,hInstance,NULL,CommandLine,SW_SHOWDEFAULT
+	invoke ExitProcess,eax
+
+end start
---
+++ Sim52/Sim52Parse.asm
@@ -0,0 +1,180 @@
+
+.code
+
+ParseList proc uses ebx esi edi,lpFileName:DWORD
+	LOCAL	hFile:HANDLE
+	LOCAL	BytesRead:DWORD
+	LOCAL	buffer[1024]:BYTE
+
+	invoke CreateFile,lpFileName,GENERIC_READ,FILE_SHARE_READ,NULL,OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,NULL
+	.if eax!=INVALID_HANDLE_VALUE
+		mov		hFile,eax
+		.if hMemFile
+			invoke GlobalFree,hMemFile
+			mov		hMemFile,0
+		.endif
+		.if hMemCode
+			invoke GlobalFree,hMemCode
+			mov		hMemCode,0
+		.endif
+		invoke GetFileSize,hFile,0
+		mov		ebx,eax
+		; Allocate memory for file
+		inc		eax
+		invoke GlobalAlloc,GMEM_FIXED or GMEM_ZEROINIT,eax
+		mov		hMemFile,eax
+		invoke GlobalAlloc,GMEM_FIXED or GMEM_ZEROINIT,65536
+		mov		hMemCode,eax
+		invoke ReadFile,hFile,hMemFile,ebx,addr BytesRead,NULL
+		invoke CloseHandle,hFile
+		mov		esi,hMemFile
+		.while byte ptr [esi]
+			call	SkipWhiteSpace
+			call	IsLineNumber
+			.if eax
+				call	SkipWhiteSpace
+				.if byte ptr [esi]>='1' && byte ptr [esi]<='9' && byte ptr [esi+1]==VK_SPACE
+					inc		esi
+					call	SkipWhiteSpace
+				.endif
+				call	IsAddress
+				.if eax
+					call	GetAddress
+					mov		edi,hMemCode
+					lea		edi,[edi+ebx]
+					call	SkipWhiteSpace
+					call	IsCodeByte
+					.if eax
+						call	GetCodeByte
+						mov		byte ptr [edi],dl
+						inc		edi
+						call	IsCodeByte
+						.if eax
+							call	GetCodeByte
+							mov		byte ptr [edi],dl
+							inc		edi
+							call	IsCodeByte
+							.if eax
+								call	GetCodeByte
+								mov		byte ptr [edi],dl
+								inc		edi
+								call	IsCodeByte
+								.if eax
+									call	GetCodeByte
+									mov		byte ptr [edi],dl
+									inc		edi
+								.endif
+							.endif
+						.endif
+					.endif
+					call	SkipWhiteSpace
+					call	GetSourceLine
+					invoke SendDlgItemMessage,hWnd,IDC_LSTCODE,LB_ADDSTRING,0,addr buffer
+					invoke SendDlgItemMessage,hWnd,IDC_LSTCODE,LB_SETITEMDATA,eax,ebx
+				.endif
+			.endif
+			call	SkipLine
+		.endw
+		xor		eax,eax
+	.endif
+	ret
+
+SkipWhiteSpace:
+	.while byte ptr [esi]==VK_SPACE || byte ptr [esi]==VK_TAB
+		inc		esi
+	.endw
+	retn
+
+SkipLine:
+	.while byte ptr [esi]!=00h && byte ptr [esi]!=0Dh && byte ptr [esi]!=0Ah
+		inc		esi
+	.endw
+	.while byte ptr [esi]==0Dh || byte ptr [esi]==0Ah
+		inc		esi
+	.endw
+	retn
+
+IsLineNumber:
+	xor		eax,eax
+	xor		ecx,ecx
+	.while byte ptr [esi]>='0' && byte ptr [esi]<='9' && ecx<=5
+		inc		ecx
+		inc		esi
+	.endw
+	.if byte ptr [esi]==':' || byte ptr [esi]=='+'
+		inc		esi
+		inc		eax
+	.endif
+	retn
+
+IsAddress:
+	xor		eax,eax
+	xor		ecx,ecx
+	.while (byte ptr [esi+ecx]>='0' && byte ptr [esi+ecx]<='9') || (byte ptr [esi+ecx]>='A' && byte ptr [esi+ecx]<='F')
+		inc		ecx
+	.endw
+	.if ecx==4
+		inc		eax
+	.endif
+	retn
+
+GetAddress:
+	xor		ecx,ecx
+	xor		ebx,ebx
+	.while ecx<4
+		movzx	eax,byte ptr [esi]
+		.if eax<='9'
+			sub		eax,'0'
+		.else
+			sub		eax,'A'-10
+		.endif
+		shl		ebx,4
+		or		ebx,eax
+		inc		esi
+		inc		ecx
+	.endw
+	retn
+
+IsCodeByte:
+	xor		eax,eax
+	xor		ecx,ecx
+	.while (byte ptr [esi+ecx]>='0' && byte ptr [esi+ecx]<='9') || (byte ptr [esi+ecx]>='A' && byte ptr [esi+ecx]<='F')
+		inc		ecx
+	.endw
+	.if ecx==2 && (byte ptr [esi+ecx]==VK_SPACE || byte ptr [esi+ecx]==VK_TAB)
+		inc		eax
+	.endif
+	retn
+
+GetCodeByte:
+	xor		ecx,ecx
+	xor		edx,edx
+	.while ecx<2
+		movzx	eax,byte ptr [esi]
+		.if eax<='9'
+			sub		eax,'0'
+		.else
+			sub		eax,'A'-10
+		.endif
+		shl		edx,4
+		or		edx,eax
+		inc		esi
+		inc		ecx
+	.endw
+	inc		esi
+	retn
+
+GetSourceLine:
+	lea		edx,buffer
+	xor		ecx,ecx
+	.while byte ptr [esi]!=00h && byte ptr [esi]!=0Dh && byte ptr [esi]!=0Ah
+		mov		al,[esi]
+		mov		[edx],al
+		inc		edx
+		inc		esi
+		inc		ecx
+	.endw
+	mov		byte ptr [edx],0
+	retn
+
+ParseList endp
