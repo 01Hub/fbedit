45a63e145e94c2d60be783ab58684e3e3227233d
ketilo <ketilo@2a7fbd6b-f62f-0410-8f40-cc077eb123af>
2011-12-19 12:37:26 +0000

    git-svn-id: https://svn.code.sf.net/p/fbedit/code@2322 2a7fbd6b-f62f-0410-8f40-cc077eb123af

--- Sim52/Release/MakeRelease.bat
+++ Sim52/Release/MakeRelease.bat
@@ -1,1 +1,8 @@
-copy ..\Addins\*.dll Sim52\Addins
+Copy ..\Addins\*.dll Sim52\Addins
+Copy ..\Sfr\*.sfr Sim52\Sfr
+Copy ..\Sim52.exe Sim52
+Copy ..\Sim52.ini Sim52
+Pause
+del ..\Sim52.zip
+"C:\Program Files\WinZip\winzip32.exe" -a -r -ex ..\Sim52.zip Sim52\*.*
+Pause
