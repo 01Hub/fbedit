ebfaa6cc4c333bb093176f401397bc0bfaa96f0b
ketilo <ketilo@2a7fbd6b-f62f-0410-8f40-cc077eb123af>
2012-02-05 19:09:56 +0000

    git-svn-id: https://svn.code.sf.net/p/fbedit/code@2389 2a7fbd6b-f62f-0410-8f40-cc077eb123af

--- STM32Discovery/TV_Terminal/main.c
+++ STM32Discovery/TV_Terminal/main.c
@@ -20,11 +20,11 @@
 * Video signal  288 lines
 *******************************************************************************/
 
-#define TOP_MARGIN                  24  // Number of lines before video signal starts
-#define SCREEN_WIDTH                64  // 80 characters on each screen line.
-#define SCREEN_HEIGHT               32  // 32 screen lines.
-#define TILE_WIDTH                  8   // Width of each character tile.
-#define TILE_HEIGHT                 8   // Height of each character tile.
+#define TOP_MARGIN                  30  // Number of lines before video signal starts
+#define SCREEN_WIDTH                40  // 40 characters on each line.
+#define SCREEN_HEIGHT               25  // 25 lines.
+#define TILE_WIDTH                  8   // Width of a character tile.
+#define TILE_HEIGHT                 10  // Height of a character tile.
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f10x_lib.h"
@@ -47,7 +47,6 @@ void NVIC_Configuration(void);
 void TIM3_Configuration(void);
 void TIM4_Configuration(void);
 void SPI_Configuration(void);
-void DMA_Configuration(void);
 void MakeVideoLine(void);
 
 /* Private functions ---------------------------------------------------------*/
@@ -65,8 +64,7 @@ int main(void)
   u8 c;
   y=0;
   c=0;
-  // while (y<SCREEN_HEIGHT)
-  while (y<16)
+  while (y<SCREEN_HEIGHT)
   {
     x=0;
     while (x<SCREEN_WIDTH)
@@ -102,14 +100,12 @@ int main(void)
   TIM_Cmd(TIM3, ENABLE);
   while (1)
   {
-    // GPIO_ResetBits(GPIOC,GPIO_Pin_8);
-    // GPIO_SetBits(GPIOC,GPIO_Pin_8);
   }
 }
 
 /*******************************************************************************
 * Function Name  : MakeVideoLine
-* Description    : This function makes a video line.
+* Description    : This function makes a single video line.
 *                  Since the SPI operates in halfword mode
 *                  odd first then even character stored in pixel buffer.
 * Input          : None
@@ -118,55 +114,23 @@ int main(void)
 *******************************************************************************/
 void MakeVideoLine(void)
 {
-  // u16 i,j,k;
-  // u8 c;
-  // j=LineCount-TOP_MARGIN;
-  // k=j && (TILE_HEIGHT-1);
-  // j=j/TILE_HEIGHT;
-  // i=0;
-  // while (i<SCREEN_WIDTH)
-  // {
-    // c=ScreenChars[j][i+1];
-    // c=Font6x8[c][k];
-    // PixelBuff[i]=c;
-    // c=ScreenChars[j][i];
-    // c=Font6x8[c][k];
-    // PixelBuff[i+1]=c;
-    // i=i+2;
-  // }
-  asm volatile("push {r4}");
-  asm volatile("mov r2,%0" : : "r" (LineCount));
-  asm volatile("sub r2,#24");
-  asm volatile("mov r4,r2");
-  asm volatile("lsr r2,#3");
-  asm volatile("lsl r2,#6");      // (LineCount>>3)<<6
-  asm volatile("and r4,#7");      // LineCount & 7
-  /* r0 current line start index into ScreenChars buffer */
-  asm volatile("mov r0,%0" : : "r" (ScreenChars));
-  asm volatile("add r0,r2");
-  /* r1 current line start pointer into Font6x8 */
-  asm volatile("mov r1,%0" : : "r" (Font6x8));
-  asm volatile("add r1,r4");
-  asm volatile("mov r2,%0" : : "r" (PixelBuff));
-  asm volatile
-  (
-    "mov r4,#0x0\r\n"             // Character index in current line
-    "L1:\r\n"
-    "add r4,#0x1\r\n"             // Odd character index
-    "ldrb r3,[r0,r4]\r\n"         // Get odd character
-    "ldrb r3,[r1,r3,lsl #3]\r\n"  // Get character tile pixels from font
-    "sub r4,#0x1\r\n"             // Even character index
-    "strb r3,[r2,r4]\r\n"         // Store in pixels buffer
-    "ldrb r3,[r0,r4]\r\n"         // Get even character
-    "ldrb r3,[r1,r3,lsl #3]\r\n"  // Get character tile pixels from font
-    "add r4,#0x1\r\n"             // Next byte
-    "strb r3,[r2,r4]\r\n"         // Store in pixels buffer
-    "add r4,#0x1\r\n"             // Next byte
-    "cmp r4,#64\r\n"
-    "it ne\r\n"
-    "bne L1"
-  );
-  asm volatile("pop {r4}");
+  u16 i,j,k;
+  u8 c;
+  j=LineCount-TOP_MARGIN;
+  k=j;
+  j=j/TILE_HEIGHT;
+  k=k-j*TILE_HEIGHT;
+  i=0;
+  while (i<SCREEN_WIDTH)
+  {
+    c=ScreenChars[j][i+1];
+    c=Font8x10[c][k];
+    PixelBuff[i]=c;
+    c=ScreenChars[j][i];
+    c=Font8x10[c][k];
+    PixelBuff[i+1]=c;
+    i=i+2;
+  }
 }
 
 /*******************************************************************************
@@ -185,28 +149,11 @@ void TIM3_IRQHandler(void)
   TIM4->CNT=0;
   /* Enable TIM4 */
   TIM4->CR1=1;
-  /* V-Sync */
+  /* H-Sync or V-Sync low */
   GPIOA->BRR=(u16)GPIO_Pin_0;
-  // /* Clear TIM3 Update interrupt pending bit */
-  // asm("mov r0,#0x0");
-  // asm("movw r1,#0x0400");
-  // asm("movt r1,#0x4000");
-  // asm("strh r0,[r1,#0x10]");
-  // /* Reset TIM4 count */
-  // asm("movw r1,#0x0800");
-  // asm("movt r1,#0x4000");
-  // asm("strh r0,[r1,#0x24]");
-  // /* Enable TIM4 */
-  // asm("mov r0,#0x1");
-  // asm("strh r0,[r1,#0x0]");
-  // /* H-Sync low */
-  // asm("movw r1,#0x0800");         // GPIOA
-  // asm("movt r1,#0x1000");
-  // asm("mov r0,#0x1");             // GPIO_Pin_0
-  // asm("strh r0,[r1,#0x14]");      // GPIO_ResetBits
-  if (LineCount>=TOP_MARGIN && LineCount<256+TOP_MARGIN)
+  if (LineCount>=TOP_MARGIN && LineCount<SCREEN_HEIGHT*TILE_HEIGHT+TOP_MARGIN)
   {
-    MakeVideoLine();
+   MakeVideoLine();
   }
 }
 
@@ -220,23 +167,41 @@ void TIM3_IRQHandler(void)
 *******************************************************************************/
 void TIM4_IRQHandler(void)
 {
-  // /* Clear TIM4 Update interrupt pending bit */
-  // asm("mov    r0,#0x0");
-  // asm("movw   r1,#0x0800");
-  // asm("movt   r1,#0x4000");
-  // asm("strh   r0,[r1,#0x10]");
-  // /* Disable TIM4 */
-  // asm("strh   r0,[r1,#0x0]");
-
   /* Disable TIM4 */
   TIM4->CR1=0;
   if (LineCount<303)
   {
     /* H-Sync high */
     GPIOA->BSRR=(u16)GPIO_Pin_0;
-    if (LineCount>=TOP_MARGIN && LineCount<256+TOP_MARGIN)
+    if (LineCount>=TOP_MARGIN && LineCount<SCREEN_HEIGHT*TILE_HEIGHT+TOP_MARGIN)
     {
-      DMA_Configuration();
+      // DMA_Configuration();
+      //Set up the DMA to keep the SPI port fed from the linebuffer.
+      // DMA_DeInit(DMA1_Channel3);
+      /* Disable the selected DMAy Channelx */
+      DMA1_Channel3->CCR &= (u32)0xFFFFFFFE;
+      /* Reset DMAy Channelx control register */
+      DMA1_Channel3->CCR  = 0;
+      /* Reset DMAy Channelx remaining bytes register */
+      DMA1_Channel3->CNDTR = 0;
+      /* Reset DMAy Channelx peripheral address register */
+      DMA1_Channel3->CPAR  = 0;
+      /* Reset DMAy Channelx memory address register */
+      DMA1_Channel3->CMAR = 0;
+      /* Reset interrupt pending bits for DMA1 Channel3 */
+      DMA1->IFCR |= (u32)0x00000F00;
+      DMA_InitStructure.DMA_PeripheralBaseAddr = (u32)0x4001300C;
+      DMA_InitStructure.DMA_MemoryBaseAddr = (u32)PixelBuff;
+      DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
+      DMA_InitStructure.DMA_Priority = DMA_Priority_VeryHigh;
+      DMA_InitStructure.DMA_BufferSize = SCREEN_WIDTH/2;
+      DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+      DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+      DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+      DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+      DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
+      DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+      DMA_Init(DMA1_Channel3, &DMA_InitStructure);
       /* Enable DMA1 Channel3 */
       DMA1_Channel3->CCR|=(u32)0x00000001;
     }
@@ -330,7 +295,7 @@ void GPIO_Configuration(void)
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_Init(GPIOC, &GPIO_InitStructure);
-  /* Sync signal */
+  /* H-Sync and V-Sync signal */
   GPIO_SetBits(GPIOA,GPIO_Pin_0);
 }
 
@@ -346,22 +311,16 @@ void NVIC_Configuration(void)
 {
   /* Set the Vector Table base location at 0x08000000 */ 
   NVIC_SetVectorTable(NVIC_VectTab_FLASH, 0x0);   
-  /* Enable the TIM2 global Interrupt */
-  NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQChannel;
-  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-  NVIC_Init(&NVIC_InitStructure);
   /* Enable the TIM3 global Interrupt */
   NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQChannel;
-  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
   NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
   NVIC_Init(&NVIC_InitStructure);
   /* Enable the TIM4 global Interrupt */
   NVIC_InitStructure.NVIC_IRQChannel = TIM4_IRQChannel;
   NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
   NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
   NVIC_Init(&NVIC_InitStructure);
 }
@@ -409,29 +368,11 @@ void SPI_Configuration(void)
 	SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
 	SPI_InitStructure.SPI_CPHA = SPI_CPHA_2Edge;
 	SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
-	SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_4;
+	SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_8;
 	SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
 	SPI_Init(SPI1, &SPI_InitStructure);
 	SPI_Cmd(SPI1, ENABLE);
 	SPI_I2S_DMACmd(SPI1, SPI_I2S_DMAReq_Tx, ENABLE);
-}
-
-void DMA_Configuration(void)
-{
-	//Set up the DMA to keep the SPI port fed from the linebuffer.
-	DMA_DeInit(DMA1_Channel3);
-	DMA_InitStructure.DMA_PeripheralBaseAddr = (u32)0x4001300C;
-	DMA_InitStructure.DMA_MemoryBaseAddr = (u32)PixelBuff;
-	DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
-	DMA_InitStructure.DMA_Priority = DMA_Priority_VeryHigh;
-	DMA_InitStructure.DMA_BufferSize = SCREEN_WIDTH/2;
-	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
-	DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
-	DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
-	DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
-	DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
-	DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
-	DMA_Init(DMA1_Channel3, &DMA_InitStructure);
 }
 
 /*****END OF FILE****/
