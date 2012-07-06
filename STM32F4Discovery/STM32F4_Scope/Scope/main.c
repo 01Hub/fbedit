de56ab4e0aa7a6d3c9daba4c887fbb235ac000ab
ketilo <ketilo@2a7fbd6b-f62f-0410-8f40-cc077eb123af>
2012-07-06 08:02:37 +0000

    git-svn-id: https://svn.code.sf.net/p/fbedit/code@2584 2a7fbd6b-f62f-0410-8f40-cc077eb123af

--- STM32F4Discovery/STM32F4_Scope/Scope/main.c
+++ STM32F4Discovery/STM32F4_Scope/Scope/main.c
@@ -36,18 +36,11 @@
 
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-//__IO uint32_t ADCTripleConvertedValue[1024];
 __IO uint16_t ADC3ConvertedValue[1500];
-// ADC_InitTypeDef           ADC_InitStructure;
-// ADC_CommonInitTypeDef     ADC_CommonInitStructure;
-// DMA_InitTypeDef           DMA_InitStructure;
-// GPIO_InitTypeDef          GPIO_InitStructure;
-// TIM_TimeBaseInitTypeDef   TIM_TimeBaseStructure;
-// TIM_OCInitTypeDef         TIM_OCInitStructure;
 
 /* Private function prototypes -----------------------------------------------*/
-//void HSC_Config(void);
 void ADC3_CH12_DMA_Config(void);
+
 /* Private functions ---------------------------------------------------------*/
 
 /**
@@ -67,8 +60,6 @@ int main(void)
 /******************************************************************************/
 /*               ADCs interface clock, pin and DMA configuration              */
 /******************************************************************************/
-  uint32_t i;
-
   ADC3_CH12_DMA_Config();
 
   /* Start ADC3 Software Conversion */ 
@@ -77,101 +68,6 @@ int main(void)
   while (1)
   {
   }
-  // /* Enable peripheral clocks */
-  // RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2 | RCC_AHB1Periph_GPIOA | RCC_AHB1Periph_GPIOC, ENABLE);
-  // RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1 | RCC_APB2Periph_ADC2 | RCC_APB2Periph_ADC3, ENABLE);
-  // RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM5, ENABLE);
-
-  // HSC_Config();
-
-  // /* Configure ADC Channel 12 pin as analog input */ 
-  // GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
-  // GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
-  // GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
-  // GPIO_Init(GPIOC, &GPIO_InitStructure);
-
-  // /* DMA2 Stream0 channel0 configuration */
-  // DMA_InitStructure.DMA_Channel = DMA_Channel_0;  
-  // DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)ADC_CDR_ADDRESS;
-  // DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)&ADCTripleConvertedValue;
-  // DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralToMemory;
-  // DMA_InitStructure.DMA_BufferSize = 900;
-  // DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
-  // DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
-  // DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Word;
-  // DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Word;
-  // DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
-  // DMA_InitStructure.DMA_Priority = DMA_Priority_VeryHigh;
-  // DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable;         
-  // DMA_InitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_HalfFull;
-  // DMA_InitStructure.DMA_MemoryBurst = DMA_MemoryBurst_Single;
-  // DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
-  // DMA_Init(DMA2_Stream0, &DMA_InitStructure);
-
-  // /* DMA2_Stream0 enable */
-  // DMA_Cmd(DMA2_Stream0, ENABLE);
-
-
-// /******************************************************************************/
-// /*  ADCs configuration: triple interleaved with 5cycles delay to reach 6Msps  */
-// /******************************************************************************/
-
-  // /* ADC Common configuration *************************************************/
-  // ADC_CommonInitStructure.ADC_Mode = ADC_TripleMode_Interl;
-  // ADC_CommonInitStructure.ADC_TwoSamplingDelay = ADC_TwoSamplingDelay_5Cycles;
-  // ADC_CommonInitStructure.ADC_DMAAccessMode = ADC_DMAAccessMode_2;  
-  // ADC_CommonInitStructure.ADC_Prescaler = ADC_Prescaler_Div8; 
-  // ADC_CommonInit(&ADC_CommonInitStructure);
-
-  // /* ADC1 regular channel 12 configuration ************************************/
-  // ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;
-  // ADC_InitStructure.ADC_ScanConvMode = DISABLE;
-  // ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
-  // ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
-  // ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
-  // ADC_InitStructure.ADC_NbrOfConversion = 1;
-
-  // /* ADC1 regular channel 12 configuration ************************************/
-  // ADC_Init(ADC1, &ADC_InitStructure);
-  // /* ADC1 regular channel12 configuration */ 
-  // ADC_RegularChannelConfig(ADC1, ADC_Channel_12, 1, ADC_SampleTime_3Cycles);
-  
-  // /* ADC2 regular channel 12 configuration ************************************/
-  // ADC_Init(ADC2, &ADC_InitStructure);
-  // /* ADC2 regular channel12 configuration */ 
-  // ADC_RegularChannelConfig(ADC2, ADC_Channel_12, 1, ADC_SampleTime_3Cycles);
-
-  // /* ADC3 regular channel 12 configuration ************************************/
-  // ADC_Init(ADC3, &ADC_InitStructure); 
-  // /* ADC3 regular channel12 configuration */ 
-  // ADC_RegularChannelConfig(ADC3, ADC_Channel_12, 1, ADC_SampleTime_3Cycles);
-
-  // /* Enable DMA request after last transfer (multi-ADC mode) ******************/
-  // ADC_MultiModeDMARequestAfterLastTransferCmd(ENABLE);
-
-  // /* Enable ADC1 **************************************************************/
-  // ADC_Cmd(ADC1, ENABLE);
-
-  // /* Enable ADC2 **************************************************************/
-  // ADC_Cmd(ADC2, ENABLE);
-
-  // /* Enable ADC3 **************************************************************/
-  // ADC_Cmd(ADC3, ENABLE);
-    
-  // /* Start ADC1 Software Conversion */ 
-  // ADC_SoftwareStartConv(ADC1);
-
-  // i=0;
-  // while (i<1000)
-  // {
-    // i++;
-  // }
-  // /* Enable ADC1 DMA */
-  // ADC_DMACmd(ADC1, ENABLE);
-
-  // while (1)
-  // {
-  // }
 }
 
 /**
@@ -181,10 +77,12 @@ int main(void)
   */
 void ADC3_CH12_DMA_Config(void)
 {
-  ADC_InitTypeDef       ADC_InitStructure;
-  ADC_CommonInitTypeDef ADC_CommonInitStructure;
-  DMA_InitTypeDef       DMA_InitStructure;
-  GPIO_InitTypeDef      GPIO_InitStructure;
+  ADC_InitTypeDef         ADC_InitStructure;
+  ADC_CommonInitTypeDef   ADC_CommonInitStructure;
+  DMA_InitTypeDef         DMA_InitStructure;
+  GPIO_InitTypeDef        GPIO_InitStructure;
+  TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+  TIM_OCInitTypeDef       TIM_OCInitStructure;
 
   /* Enable ADC3, DMA2 and GPIO clocks ****************************************/
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2 | RCC_AHB1Periph_GPIOC, ENABLE);
@@ -242,46 +140,47 @@ void ADC3_CH12_DMA_Config(void)
 
   /* Enable ADC3 */
   ADC_Cmd(ADC3, ENABLE);
+
+  /* Time base configuration */
+  TIM_TimeBaseStructure.TIM_Period = 83;
+  TIM_TimeBaseStructure.TIM_Prescaler = 0;
+  TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+  TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;
+  TIM_TimeBaseInit(TIM5, &TIM_TimeBaseStructure);
+  /* PWM1 Mode configuration: Channel3 */
+  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_OutputNState = TIM_OutputState_Disable;
+  TIM_OCInitStructure.TIM_Pulse = 41;
+  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+  TIM_OCInitStructure.TIM_OCNPolarity = TIM_OCPolarity_Low;
+  TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Reset;
+  TIM_OCInitStructure.TIM_OCNIdleState = TIM_OCIdleState_Reset;
+  TIM_OC3Init(TIM5, &TIM_OCInitStructure);
+
+  TIM_OC1PreloadConfig(TIM5, TIM_OCPreload_Enable);
+  TIM_ARRPreloadConfig(TIM5, ENABLE);
+  /* TIM5 Main Output Enable */
+  TIM_CtrlPWMOutputs(TIM5, ENABLE);
+  /* TIM5 chennel 3 configuration : PA.02 */
+  GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_2;
+  GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL ;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  
+  /* Connect TIM5 pin to AF2 */
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_TIM5);
+
+  /* TIM5 enable counter */
+  TIM_Cmd(TIM5, ENABLE);
 }
 
-// void HSC_Config(void)
-// {
-  // /* Time base configuration */
-  // TIM_TimeBaseStructure.TIM_Period = 83;
-  // TIM_TimeBaseStructure.TIM_Prescaler = 0;
-  // TIM_TimeBaseStructure.TIM_ClockDivision = 0;
-  // TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
-  // TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;
-  // TIM_TimeBaseInit(TIM5, &TIM_TimeBaseStructure);
-  // /* PWM1 Mode configuration: Channel3 */
-  // TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
-  // TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-  // TIM_OCInitStructure.TIM_OutputNState = TIM_OutputState_Disable;
-  // TIM_OCInitStructure.TIM_Pulse = 41;
-  // TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
-  // TIM_OCInitStructure.TIM_OCNPolarity = TIM_OCPolarity_Low;
-  // TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Reset;
-  // TIM_OCInitStructure.TIM_OCNIdleState = TIM_OCIdleState_Reset;
-  // TIM_OC3Init(TIM5, &TIM_OCInitStructure);
-
-  // TIM_OC1PreloadConfig(TIM5, TIM_OCPreload_Enable);
-  // TIM_ARRPreloadConfig(TIM5, ENABLE);
-  // /* TIM5 Main Output Enable */
-  // TIM_CtrlPWMOutputs(TIM5, ENABLE);
-  // /* TIM5 chennel 3 configuration : PA.02 */
-  // GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_2;
-  // GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_AF;
-  // GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  // GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  // GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL ;
-  // GPIO_Init(GPIOA, &GPIO_InitStructure);
-  
-  // /* Connect TIM5 pin to AF2 */
-  // GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_TIM5);
-
-  // /* TIM5 enable counter */
-  // TIM_Cmd(TIM5, ENABLE);
-// }
+void HSC_Config(void)
+{
+}
 
 #ifdef  USE_FULL_ASSERT
 
