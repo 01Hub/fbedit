1facf162e8678edc7dd582d94cfcab6523859b2c
ketilo <ketilo@2a7fbd6b-f62f-0410-8f40-cc077eb123af>
2012-08-10 14:24:17 +0000

    git-svn-id: https://svn.code.sf.net/p/fbedit/code@2629 2a7fbd6b-f62f-0410-8f40-cc077eb123af

--- PhotoFrame/STM32F4/main.c
+++ PhotoFrame/STM32F4/main.c
@@ -13,10 +13,16 @@
 /* Private typedef -----------------------------------------------------------*/
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
+NVIC_InitTypeDef NVIC_InitStructure;
+uint16_t x;
+uint16_t y;
+uint16_t pix;
+uint8_t pixarray[480*3][5]; // Should be [480*3][234]
 /* Private function prototypes -----------------------------------------------*/
 void RCC_Config(void);
 void NVIC_Config(void);
 void GPIO_Config(void);
+void TIM_Config(void);
 /* Private functions ---------------------------------------------------------*/
 
 /*******************************************************************************
@@ -26,12 +32,13 @@ void GPIO_Config(void);
 * Output         : None
 * Return         : None
 *******************************************************************************/
-int main(void)
+void main(void)
 {
   asm("add  sp,#0x10000");
 
   RCC_Config();
   GPIO_Config();
+  TIM_Config();
   NVIC_Config();
   while (1)
   {
@@ -40,16 +47,20 @@ int main(void)
 
 void RCC_Config(void)
 {
-  /* Enable GPIOA, GPIOB, GPIOC and GPIOE clocks ****************************************/
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA | RCC_AHB1Periph_GPIOB | RCC_AHB1Periph_GPIOC | RCC_AHB1Periph_GPIOE, ENABLE);
+  /* Enable TIM2, GPIOA and GPIOE clocks */
+  RCC_AHB1PeriphClockCmd(RCC_APB1Periph_TIM2 | RCC_AHB1Periph_GPIOA | RCC_AHB1Periph_GPIOE, ENABLE);
   /* Enable SYSCFG clock */
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
 }
 
 void NVIC_Config(void)
 {
-  NVIC_InitTypeDef NVIC_InitStructure;
-
+  /* Enable the TIM2 gloabal Interrupt */
+  NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
   /* Enable and set EXTI Line9_5 Interrupt to the lowest priority */
   NVIC_InitStructure.NVIC_IRQChannel = EXTI9_5_IRQn;
   NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x01;
@@ -63,32 +74,123 @@ void GPIO_Config(void)
   GPIO_InitTypeDef        GPIO_InitStructure;
   EXTI_InitTypeDef        EXTI_InitStructure;
 
-  /* GPIOE Pin15 to Pin7 as outputs */
-  GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_15 | GPIO_Pin_14 | GPIO_Pin_13 | GPIO_Pin_12 | GPIO_Pin_11 | GPIO_Pin_10 | GPIO_Pin_9 | GPIO_Pin_8 | GPIO_Pin_7;
+  /* GPIOE Pin15 to Pin6 as outputs */
+  GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_15 | GPIO_Pin_14 | GPIO_Pin_13 | GPIO_Pin_12 | GPIO_Pin_11 | GPIO_Pin_10 | GPIO_Pin_9 | GPIO_Pin_8 | GPIO_Pin_7 | GPIO_Pin_6;
   GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_OUT;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL ;
   GPIO_Init(GPIOE, &GPIO_InitStructure);
-  /* GPIOE Pin6 and Pin5 as input floating */
+  /* GPIOE Pin5 and Pin4 as input floating */
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_5;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_4;
   GPIO_Init(GPIOE, &GPIO_InitStructure);
+
+  /* Connect EXTI Line4 to PE4 pin */
+  SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE, EXTI_PinSource4);
+  /* Configure EXTI Line4 */
+  EXTI_InitStructure.EXTI_Line = EXTI_Line4;
+  EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+  EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;  
+  EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+  EXTI_Init(&EXTI_InitStructure);
 
   /* Connect EXTI Line5 to PE5 pin */
   SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE, EXTI_PinSource5);
-
   /* Configure EXTI Line5 */
   EXTI_InitStructure.EXTI_Line = EXTI_Line5;
   EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
   EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;  
   EXTI_InitStructure.EXTI_LineCmd = ENABLE;
   EXTI_Init(&EXTI_InitStructure);
+
+  /* TIM2 channel 2 configuration : PA1 */
+  GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_1;
+  GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL ;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  /* Connect TIM2 pin to AF2 */
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource1, GPIO_AF_TIM2);
 }
 
+void TIM_Config(void)
+{
+  TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+
+  /* TIM2 Counter configuration */
+  TIM_TimeBaseStructure.TIM_Period = 0x7;
+  TIM_TimeBaseStructure.TIM_Prescaler = 0;
+  TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+  TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+  TIM2->CCMR1 = 0x0100;     //CC2S=01
+  TIM2->SMCR = 0x0067;      //TS=110, SMS=111
+}
+
+/**
+  * @brief  This function handles TIM2 global interrupt request.
+            The interrupt is generated for every 8 CLK pulses
+  * @param  None
+  * @retval None
+  */
+void TIM2_IRQHandler(void)
+{
+  TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+  pix = pixarray[x][y] << 8 | 0x80;
+  GPIOE->ODR = pix;
+  pix = pixarray[x+1][y] << 8;
+  pix = pixarray[x+1][y] << 8;
+  x+=3;
+  if (x = 480*3)
+  {
+    /* Disable TIM2 */
+    TIM_Cmd(TIM2, DISABLE);
+  }
+}
+
+/**
+  * @brief  This function handles EXTI4_IRQHandler interrupt request.
+            The interrupt is generated on STHL transition
+  * @param  None
+  * @retval None
+  */
+void EXTI4_IRQHandler(void)
+{
+  /* Clear the EXTI line 4 pending bit */
+  EXTI_ClearITPendingBit(EXTI_Line4);
+  /* Increment line counter */
+  y++;
+  if (y >= 2 && y <= 234+2)
+  {
+    /* Reset pixel byte counter */
+    x = 0;
+    /* Enable TIM2 */
+    TIM_Cmd(TIM2, ENABLE);
+  }
+}
+
+/**
+  * @brief  This function handles EXTI9_5_IRQHandler interrupt request.
+            The interrupt is generated on VSync transition
+  * @param  None
+  * @retval None
+  */
 void EXTI9_5_IRQHandler(void)
 {
-  /* Clear the EXTI line 5 pending bit */
-  EXTI_ClearITPendingBit(EXTI_Line5);
+  if(EXTI_GetITStatus(EXTI_Line5) != RESET)
+  {
+    /* Clear the EXTI line 5 pending bit */
+    EXTI_ClearITPendingBit(EXTI_Line5);
+    /* Reset line counter */
+    y = 0;
+    /* Enable and set EXTI Line4 Interrupt to the lowest priority */
+    NVIC_InitStructure.NVIC_IRQChannel = EXTI4_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x01;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x01;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+  }
 }
