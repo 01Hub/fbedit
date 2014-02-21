d2b04f611e76ad5ea09c38294d2d87abf1f9d739
ketilo <ketilo@2a7fbd6b-f62f-0410-8f40-cc077eb123af>
2014-02-21 06:29:05 +0000

    git-svn-id: https://svn.code.sf.net/p/fbedit/code@2927 2a7fbd6b-f62f-0410-8f40-cc077eb123af

---
+++ STM32F4Discovery/STM32F4_LCMeter/main.c
@@ -0,0 +1,214 @@
+/**
+  ******************************************************************************
+  * @file    LCMeter/main.c 
+  * @author  MCD Application Team / KO
+  * @version V1.0.0
+  * @date    20-February-2014
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+  *
+  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
+  ******************************************************************************
+  */ 
+
+/**
+  ******************************************************************************
+  Port pins
+  PA.01         Frequency counter input
+  PD.00         Frequency counter select0
+  PD.01         Frequency counter select1
+  PD.02         Frequency counter select2
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4_discovery.h"
+
+/* Private typedef -----------------------------------------------------------*/
+typedef struct
+{
+  uint32_t Frequency;                           // 0x20000014
+  uint32_t PreviousCount;
+  uint32_t ThisCount;
+  uint32_t TickCount;
+}STM32_FRQTypeDef;
+
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+static STM32_FRQTypeDef STM32_FRQ;              // 0x20000014
+
+/* Private function prototypes -----------------------------------------------*/
+void RCC_Config(void);
+void NVIC_Config(void);
+void GPIO_Config(void);
+void TIM_Config(void);
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  /*!< At this stage the microcontroller clock setting is already configured, 
+       this is done through SystemInit() function which is called from startup
+       file (startup_stm32f4xx.s) before to branch to application main.
+       To reconfigure the default setting of SystemInit() function, refer to
+       system_stm32f4xx.c file
+     */
+
+  /* RCC Configuration */
+  RCC_Config();
+  /* GPIO Configuration */
+  GPIO_Config();
+  /* TIM Configuration */
+  TIM_Config();
+  /* NVIC Configuration */
+  NVIC_Config();
+  while (1);
+}
+
+/**
+  * @brief  Configure the RCC.
+  * @param  None
+  * @retval None
+  */
+void RCC_Config(void)
+{
+  /* TIM2 and TIM3 clock enable */
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2 | RCC_APB1Periph_TIM3, ENABLE);
+  /* GPIOA clock enable */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+  /* GPIOD clock enable */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+}
+
+/**
+  * @brief  Configure the NVIC.
+  * @param  None
+  * @retval None
+  */
+void NVIC_Config(void)
+{
+  NVIC_InitTypeDef NVIC_InitStructure;
+  /* Enable the TIM3 gloabal Interrupt */
+  NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+}
+
+/**
+  * @brief  Configure the GPIO.
+  * @param  None
+  * @retval None
+  */
+void GPIO_Config(void)
+{
+  GPIO_InitTypeDef GPIO_InitStructure;
+  /* Initialize Leds mounted on STM32F4-Discovery board */
+  STM_EVAL_LEDInit(LED3);
+  /* TIM2 chennel2 configuration : PA.01 */
+  GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_1;
+  GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  /* Connect TIM2 pin to AF2 */
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource1, GPIO_AF_TIM2);
+  /* GPIOD */
+  GPIO_ResetBits(GPIOD, GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2);
+  GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2;
+  GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOD, &GPIO_InitStructure);
+}
+
+/**
+  * @brief  Configure the TIM IRQ Handler.
+  * @param  None
+  * @retval None
+  */
+void TIM_Config(void)
+{
+  TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+  /* TIM2 Counter configuration */
+  TIM_TimeBaseStructure.TIM_Period = 0xffffffff;
+  TIM_TimeBaseStructure.TIM_Prescaler = 0;
+  TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+  TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+  TIM2->CCMR1 = 0x0100;     //CC2S=01
+  TIM2->SMCR = 0x0067;      //TS=110, SMS=111
+  /* TIM3 1 second Time base configuration */
+  TIM_TimeBaseStructure.TIM_Period = 9999;
+  TIM_TimeBaseStructure.TIM_Prescaler = 8399;
+  TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+  TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
+  /* TIM Interrupts enable */
+  TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
+  /* TIM2 enable counter */
+  TIM_Cmd(TIM2, ENABLE);
+  /* TIM3 enable counter */
+  TIM_Cmd(TIM3, ENABLE);
+}
+
+/**
+  * @brief  This function handles TIM3 global interrupt request.
+  * @param  None
+  * @retval None
+  */
+void TIM3_IRQHandler(void)
+{
+  STM32_FRQ.ThisCount = TIM2->CNT;
+  STM32_FRQ.Frequency = STM32_FRQ.ThisCount - STM32_FRQ.PreviousCount;
+  STM32_FRQ.PreviousCount = STM32_FRQ.ThisCount;
+  STM32_FRQ.TickCount++;
+  TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
+  STM_EVAL_LEDToggle(LED3);
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  while (1)
+  {}
+}
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */ 
+
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
