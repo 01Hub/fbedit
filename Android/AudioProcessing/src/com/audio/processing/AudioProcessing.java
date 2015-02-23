8913b9ddca28572cb66b9e508ca2b4b6a6cf0fca
ketilo <ketilo@2a7fbd6b-f62f-0410-8f40-cc077eb123af>
2015-02-23 14:27:40 +0000

    git-svn-id: https://svn.code.sf.net/p/fbedit/code@3051 2a7fbd6b-f62f-0410-8f40-cc077eb123af

--- Android/AudioProcessing/src/com/audio/processing/AudioProcessing.java
+++ Android/AudioProcessing/src/com/audio/processing/AudioProcessing.java
@@ -24,12 +24,15 @@ public class AudioProcessing extends Activity implements OnClickListener{
     private RealDoubleFFT transformer;
     int blockSize = 256;
     Button startStopButton;
+    Button downButton;
+    Button upButton;
     boolean started = false;
     RecordAudio recordTask;
     ImageView imageView;
     Bitmap bitmap;
     Canvas canvas;
     Paint paint;
+    int frq=100;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,6 +40,27 @@ public class AudioProcessing extends Activity implements OnClickListener{
         setContentView(R.layout.main);
         startStopButton = (Button) this.findViewById(R.id.StartStopButton);
         startStopButton.setOnClickListener(this);
+
+        downButton = (Button) this.findViewById(R.id.button2);
+        downButton.setOnClickListener(this);
+        downButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+            	frq += 1;
+            	Log.e("AudioRecord", "downButton " + frq);
+			}
+		});
+
+        upButton = (Button) this.findViewById(R.id.button1);
+        upButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+            	frq -= 1;
+            	Log.e("AudioRecord", "upButton " + frq);
+			}
+		});
+
+
         transformer = new RealDoubleFFT(blockSize);
         imageView = (ImageView) this.findViewById(R.id.ImageView01);
         bitmap = Bitmap.createBitmap((int)256,(int)200,Bitmap.Config.ARGB_8888);
@@ -50,21 +74,30 @@ public class AudioProcessing extends Activity implements OnClickListener{
         @Override
         protected Void doInBackground(Void... params) {
         	try {
-        		int bufferSize = AudioRecord.getMinBufferSize(frequency, channelConfiguration, audioEncoding);
-                AudioRecord audioRecord = new AudioRecord(MediaRecorder.AudioSource.DEFAULT, frequency, channelConfiguration, audioEncoding, bufferSize);
+//        		int bufferSize = AudioRecord.getMinBufferSize(frequency, channelConfiguration, audioEncoding);
+//                AudioRecord audioRecord = new AudioRecord(MediaRecorder.AudioSource.DEFAULT, frequency, channelConfiguration, audioEncoding, bufferSize);
                 short[] buffer = new short[blockSize];
                 double[] toTransform = new double[blockSize];
-                audioRecord.startRecording();
+//                audioRecord.startRecording();
                 while (started) {
-                	int bufferReadResult = audioRecord.read(buffer, 0, blockSize);
+//                	int bufferReadResult = audioRecord.read(buffer, 0, blockSize);
  //                   Log.e("AudioRecord", "Recording: " + bufferReadResult);
-                	for (int i = 0; i < blockSize && i < bufferReadResult; i++) {
-                		toTransform[i] = (double) buffer[i] / 32768.0; // signed 16 bit
+//                	for (int i = 0; i < blockSize && i < bufferReadResult; i++) {
+//                		toTransform[i] = (double) buffer[i] / 32768.0; // signed 16 bit
+//                	}
+                	for (int i = 0; i < blockSize; i++) {
+                		toTransform[i] = ((float) Math.sin( (float)i * ((float)(2*Math.PI) * frq / 8000)))/15.0 + ((float) Math.sin( (float)i * ((float)(2*Math.PI) * frq*2 / 8000)))/30.0 + ((float) Math.sin( (float)i * ((float)(2*Math.PI) * frq*3 / 8000)))/30.0;    //the part that makes this a sine wave.... 
+                				//0.001D*(i & 7);//(double) buffer[i] / 32768.0; // signed 16 bit
                 	}
                 	transformer.ft(toTransform);
                 	publishProgress(toTransform);
+
+                	for (int i = 0; i < 1000000; i++) {
+                	}
+                	
+//                	Log.e("AudioRecord", "Recording: ");
                 }
-                audioRecord.stop();
+//                audioRecord.stop();
             } catch (Throwable t) {
                 Log.e("AudioRecord", "Recording Failed");
             }
@@ -78,7 +111,7 @@ public class AudioProcessing extends Activity implements OnClickListener{
 	        canvas.drawColor(Color.BLACK);
 	        for (int i = 0; i < toTransform[0].length; i++) {
 	        	x = i;
-	        	downy = (int) (100 - (toTransform[0][i] * 100));
+	        	downy = (int) (100 - (Math.abs(toTransform[0][i]*10D)));
 	        	int upy = 100;
 	        	canvas.drawLine(x, downy, x, upy, paint);
 	        }
