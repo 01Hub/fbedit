b105dd8f32047b52b368407dc80916301621f18f
ketilo <ketilo@2a7fbd6b-f62f-0410-8f40-cc077eb123af>
2015-03-03 12:55:26 +0000

    git-svn-id: https://svn.code.sf.net/p/fbedit/code@3059 2a7fbd6b-f62f-0410-8f40-cc077eb123af

--- Android/VideoPlay/gen/com/example/videoplay/R.java
+++ Android/VideoPlay/gen/com/example/videoplay/R.java
@@ -12,6 +12,10 @@ public final class R {
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
+
+         Customize dimensions originally defined in res/values/dimens.xml (such as
+         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+    
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
@@ -20,10 +24,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int myVideo=0x7f070000;
+        public static final int action_settings=0x7f080005;
+        public static final int btn0=0x7f080000;
+        public static final int btn1=0x7f080001;
+        public static final int btn2=0x7f080002;
+        public static final int btn3=0x7f080003;
+        public static final int myVideo=0x7f080004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int vidmain=0x7f030001;
+    }
+    public static final class menu {
+        public static final int main=0x7f070000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -40,6 +53,17 @@ public final class R {
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
+
+        Base application theme for API 11+. This theme completely replaces
+        AppBaseTheme from res/values/styles.xml on API 11+ devices.
+    
+ API 11 theme customizations can go here. 
+
+        Base application theme for API 14+. This theme completely replaces
+        AppBaseTheme from BOTH res/values/styles.xml and
+        res/values-v11/styles.xml on API 14+ devices.
+    
+ API 14 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
--- Android/VideoPlay/src/com/example/videoplay/MainActivity.java
+++ Android/VideoPlay/src/com/example/videoplay/MainActivity.java
@@ -5,14 +5,23 @@ import java.io.File;
 import android.os.Bundle;
 import android.os.Environment;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.net.Uri;
+import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class MainActivity extends Activity {
 
+	private int rseed;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,13 +31,80 @@ public class MainActivity extends Activity {
 		WindowManager.LayoutParams.FLAG_FULLSCREEN,  
 		WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.main);
-		VideoView vidView = (VideoView)findViewById(R.id.myVideo);
-		Uri vidUri = Uri.parse(Environment.getExternalStorageDirectory() + File.separator + "Movies" + File.separator + "video.mp4");
+
+		final MediaController vidControl = new MediaController(this);
+		Button btn0 = (Button) this.findViewById(R.id.btn0);
+		Button btn1 = (Button) this.findViewById(R.id.btn1);
+		Button btn2 = (Button) this.findViewById(R.id.btn2);
+		Button btn3 = (Button) this.findViewById(R.id.btn3);
+		rseed=1234;
+		btn0.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				PlayVideo(vidControl, 0);
+			 }
+		});
+		btn1.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				PlayVideo(vidControl, 1);
+			 }
+		});
+		btn2.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				PlayVideo(vidControl, 2);
+			 }
+		});
+		btn3.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				PlayVideo(vidControl, 3);
+			 }
+		});
+	}
+
+	private void PlayVideo(MediaController vidControl,int btn) {
+		setContentView(R.layout.vidmain);
+		final VideoView vidView = (VideoView)findViewById(R.id.myVideo);
+		String path;
+		String file = "";
+		Uri vidUri = null;
+		path = Environment.getExternalStorageDirectory() + File.separator + "Movies" + File.separator;
+		switch (btn) {
+		case 0:
+			file = "video.mp4";
+		case 1:
+			file = "video.mp4";
+		case 2:
+			file = "video.mp4";
+		case 3:
+			file = "video.mp4";
+		}
+		vidUri = Uri.parse(path + file);
 
 		vidView.setVideoURI(vidUri);
-		MediaController vidControl = new MediaController(this);
 		vidControl.setAnchorView(vidView);
 		vidView.setMediaController(vidControl);
+	   vidView.setOnCompletionListener(new OnCompletionListener() {
+	        @Override
+	        public void onCompletion(MediaPlayer mp) {
+				Intent i = getBaseContext().getPackageManager().getLaunchIntentForPackage( getBaseContext().getPackageName() );
+				i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivity(i);
+			}
+	    });
 		vidView.start();
+	}
+	
+	private int Random() {
+		int lsb;
+		int rnd = rseed * 23 + 7;
+		lsb = (rnd & 1) << 31;
+		rnd >>>= 1;
+		rnd |= lsb;
+		rnd ^= rseed;
+		rseed = rnd;
+		return rnd;
 	}
 }
